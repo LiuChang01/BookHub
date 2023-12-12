45d2fccc5ec46adb60c146f7fda55e017c5f141e
LiuChang01 <c.liu4@studenti.unipi.it>
2023-12-12 18:24:42 +0100

    change

--- BookHub/src/main/java/it/unipi/lsmsd/bookhub/BookHubApplication.java
+++ BookHub/src/main/java/it/unipi/lsmsd/bookhub/BookHubApplication.java
@@ -1,13 +1,15 @@
-package it.unipi.lsmsd.BookHub;
+package it.unipi.lsmsd.bookhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class BookHubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookHubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookHubApplication.class, args);
+    }
 
 }
