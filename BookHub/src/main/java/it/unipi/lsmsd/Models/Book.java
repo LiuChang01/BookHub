6d8254ab43123196ba298eeb3f34ca3ad16d3b48
LiuChang01 <c.liu4@studenti.unipi.it>
2023-12-12 21:11:43 +0100

    added some class

---
+++ BookHub/src/main/java/it/unipi/lsmsd/Models/Book.java
@@ -0,0 +1,8 @@
+package it.unipi.lsmsd.Models;
+
+public class Book {
+    private Author author;
+    private String ISBN;
+    private String genre;
+    
+}
---
+++ BookHub/src/main/java/it/unipi/lsmsd/Models/User.java
@@ -0,0 +1,4 @@
+package it.unipi.lsmsd.Models;
+
+public class User {
+}
