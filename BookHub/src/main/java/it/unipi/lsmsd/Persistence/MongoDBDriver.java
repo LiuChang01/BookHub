aa01b741b193025b2b04f6f3fefe0bdd3906891c
LiuChang01 <c.liu4@studenti.unipi.it>
2023-12-16 17:06:47 +0100

    ok

--- BookHub/src/main/java/it/unipi/lsmsd/Persistence/MongoDBDriver.java
+++ BookHub/src/main/java/it/unipi/lsmsd/Persistence/MongoDBDriver.java
@@ -1,6 +1,10 @@
 package it.unipi.lsmsd.Persistence;
 
 import com.mongodb.client.MongoClient;
+import it.unipi.lsmsd.Utils.Utils;
+
+import java.util.Objects;
+import java.util.Properties;
 
 public class MongoDBDriver {
     private static MongoDBDriver instance;
@@ -14,5 +18,31 @@ public class MongoDBDriver {
     public String mongoThirdIp;
     public int mongoThirdPort;
     public String mongodbName;
+     private MongoDBDriver(Properties configurationParameters){
+         this.mongoUsername = configurationParameters.getProperty("mongoUsername");
+         this.mongoPassword = configurationParameters.getProperty("mongoPassword");
+         this.mongoFirstIp = configurationParameters.getProperty("mongoFirstIp");
+         this.mongoFirstPort = Integer.parseInt(configurationParameters.getProperty("mongoFirstPort"));
+         this.mongoSecondIp = configurationParameters.getProperty("mongoSecondIp");
+         this.mongoSecondPort = Integer.parseInt(configurationParameters.getProperty("mongoSecondPort"));
+         this.mongoThirdIp = configurationParameters.getProperty("mongoThirdIp");
+         this.mongoThirdPort = Integer.parseInt(configurationParameters.getProperty("mongoThirdPort"));
+         this.mongodbName = configurationParameters.getProperty("mongoDbName");
+     }
+    public static MongoDBDriver getInstance() {
+        if (instance == null)
+            instance = new MongoDBDriver(Objects.requireNonNull(Utils.readConfigurationParameters()));
+        return instance;
+    }
+    public void closeConnection() {
+        if (client != null)
+            System.out.println("Connection closed ...");
+        client.close();
+    }
+    public MongoClient openConnection(){
+         if(client!=null){
+             return client;
+         }
 
+    }
 }
