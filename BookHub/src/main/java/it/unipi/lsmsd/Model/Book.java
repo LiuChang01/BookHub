b3788a594dc239b72002a39a5cd741df53eb2181
LiuChang01 <c.liu4@studenti.unipi.it>
2023-12-16 16:10:06 +0100

    create models

--- BookHub/src/main/java/it/unipi/lsmsd/Model/Book.java
+++ BookHub/src/main/java/it/unipi/lsmsd/Model/Book.java
@@ -1,4 +1,70 @@
 package it.unipi.lsmsd.Model;
 
+import java.util.List;
+
 public class Book {
+    private String ISBN;
+    private String Title;
+    private String description;
+    List<String>authors;
+    List<String>categories;
+
+    public Book(String ISBN, String title, String description, List<String> authors, List<String> categories) {
+        this.ISBN = ISBN;
+        Title = title;
+        this.description = description;
+        this.authors = authors;
+        this.categories = categories;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "ISBN='" + ISBN + '\'' +
+                ", Title='" + Title + '\'' +
+                ", description='" + description + '\'' +
+                ", authors=" + authors +
+                ", categories=" + categories +
+                '}';
+    }
 }
--- BookHub/src/main/java/it/unipi/lsmsd/Model/User.java
+++ BookHub/src/main/java/it/unipi/lsmsd/Model/User.java
@@ -1,4 +1,53 @@
 package it.unipi.lsmsd.Model;
 
+import org.neo4j.fabric.planning.Use;
+
 public class User {
+    private String username;
+    private String password;
+    private int type;
+    public User(){}
+
+    public User(String username, String password, int type) {
+        this.username = username;
+        this.password = password;
+        this.type = type;
+    }
+
+    public User(String username, String password) {
+        this(username,password,0);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", type=" + type +
+                '}';
+    }
 }
